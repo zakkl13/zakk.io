d88a7b75f51691bf53bbafa91bf593170164b9d0
Zakk <zakkl13@vt.edu>
2015-10-21 22:43:22 -0400

    Fixed directory issues after renaming project directory

--- zakkio/wsgi.py
+++ zakkio/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for DJANGO project.
+WSGI config for zakkio project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "DJANGO.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "zakkio.settings")
 
 application = get_wsgi_application()
