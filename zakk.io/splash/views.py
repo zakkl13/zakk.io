65c3542e0a9723c9bcf340d002311aa5cd556dec
Zakk <zakkl13@vt.edu>
2015-10-22 18:31:37 -0400

    Moved gitignore to top dir, updated splash app to match static page

--- zakk.io/splash/views.py
+++ zakk.io/splash/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import ShortText
+
 # Create your views here.
 def index(request):
-	return HttpResponse("This is the splash page index!")
+	#Random code
+	Noun = 'penguins'
+	Action = 'fantasize about'
+	return render(request, 'splash/index.html', {'action': Action, 'noun': Noun})
+	#return HttpResponse("This is the splash page index!")
